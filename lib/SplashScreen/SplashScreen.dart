1a628aabc5193f84f89e9923293f80f69b2fce8b
aryanbhimani <aryan.bhimani.93@gmail.com>
2024-12-27 22:44:19 +0530

    new SplashScreen

---
+++ lib/SplashScreen/SplashScreen.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration.zero, () {
+      final User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        // Navigate to Home if user is logged in
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Home()),
+        );
+      } else {
+        // Navigate to Login if user is not logged in
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const OnboardingPage()),
+        );
+      }
+    });
+
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class OnboardingInfo {
+  final String title;
+  final String description;
+  final String image;
+
+  OnboardingInfo({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
+
+// OnboardingData class
+class OnboardingData {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+      title: "NoteEase",
+      description: "Capture, sync, and organize your notes in real-time.",
+      image: "assets/onboarding1.png",
+    ),
+    OnboardingInfo(
+      title: "MyNotes",
+      description: "Your notes, always synced and accessible, wherever you are.",
+      image: "assets/onboarding2.png",
+    ),
+    OnboardingInfo(
+      title: "QuickNotes",
+      description: "Effortless note management with real-time syncing and cloud storage",
+      image: "assets/onboarding3.png",
+    ),
+  ];
+}
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  final controller = OnboardingData();
+  final pageController = PageController();
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkOnboardingCompleted();
+  }
+
+  // Check if onboarding is completed
+  Future<void> _checkOnboardingCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isOnboardingCompleted = prefs.getBool('isOnboardingCompleted') ?? false;
+
+    if (isOnboardingCompleted) {
+      // Navigate to the login page if onboarding is completed
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Login()),
+      );
+    }
+  }
+
+  // Set onboarding as completed
+  Future<void> _setOnboardingCompleted() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isOnboardingCompleted', true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          body(),
+          buildDots(),
+          button(),
+        ],
+      ),
+    );
+  }
+
+  // Body
+  Widget body() {
+    return Expanded(
+      child: Center(
+        child: PageView.builder(
+          onPageChanged: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          itemCount: controller.items.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Images
+                  Image.asset(controller.items[currentIndex].image),
+
+                  const SizedBox(height: 15),
+                  // Titles
+                  Text(
+                    controller.items[currentIndex].title,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      color: Color(0xFFFFCA28),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+
+                  // Description
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Text(
+                      controller.items[currentIndex].description,
+                      style: const TextStyle(color: Colors.grey, fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  // Dots
+  Widget buildDots() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(controller.items.length, (index) {
+        return AnimatedContainer(
+          margin: const EdgeInsets.symmetric(horizontal: 2),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: currentIndex == index ? Color(0xFFFFCA28) : Colors.grey,
+          ),
+          height: 7,
+          width: currentIndex == index ? 30 : 7,
+          duration: const Duration(milliseconds: 700),
+        );
+      }),
+    );
+  }
+
+  // Button
+  Widget button() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      width: MediaQuery.of(context).size.width * .9,
+      height: 55,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Color(0xFFFFCA28),
+      ),
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            if (currentIndex != controller.items.length - 1) {
+              currentIndex++;
+            } else {
+              // Mark onboarding as completed and navigate to login page
+              _setOnboardingCompleted();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => Login()),
+              );
+            }
+          });
+        },
+        child: Text(
+          currentIndex == controller.items.length - 1
+              ? "Get started"
+              : "Continue",
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
