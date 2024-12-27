92283d8a44623d42c68105f3b0bacdb246f3a915
aryanbhimani <aryan.bhimani.93@gmail.com>
2024-12-27 22:48:15 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:weatherapp/SplashScreen/SplashScreen.dart';
 import 'package:weatherapp/home/Home.dart';
 
 void main() async{
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Weather App Home Page'),
+      // home: const MyHomePage(title: 'Weather App Home Page'),
+      home: const SplashScreen(),
     );
   }
 }
