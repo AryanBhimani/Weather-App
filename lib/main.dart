a6d07a72796933c4d0843c4f5e8bea1b16f906df
aryanbhimani <aryan.bhimani.93@gmail.com>
2024-12-28 10:52:12 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:weatherapp/SplashScreen/SplashScreen.dart';
+import 'package:weatherapp/View/SplashScreen/SplashScreen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
